9a4623ee420fa0b7200757c26c00f63f0bab19ac
Valentin Valchev <v_valchev@developer.bg>
2020-03-03 10:23:16 +0200

    Fixing the issue #1
    The problem is with github-pages, which enables only safe plugins. So the ones in _plugins folders are not executed. In that case, you have to manually edit the _data/menus.yml file.
    
    For consistency, this change disables unsafe plugins, just like github-pages and does it manually.
    
    Anyway, enabling unsafe plugins will run the hook, but it's modified to skip entries, that are already added manually.

--- Gemfile
+++ Gemfile
@@ -2,14 +2,10 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "jekyll", "~> 3.8.4"
+gem "github-pages", "~> 204"
 
+# Other dependencies as paginate, sitemap and 'seo-tag'
+# are already added by github-pages
 group :jekyll_plugins do
-    gem "jekyll-menus", "~> 0.6.0"
-    gem "jekyll-paginate-v2", "~> 1.9"
-    gem "jekyll-paginate", "~> 1.1"
+    gem "jekyll-menus", "~> 0.6.1"
 end
-
-gem "jekyll-sitemap", "~> 1.4"
-
-gem "jekyll-seo-tag", "~> 2.6"
--- _plugins/hooks.rb
+++ _plugins/hooks.rb
@@ -1,15 +1,37 @@
-# Load all sections, that has menu_title into the 'header' menu
+# WARNING: This script will not be executed on github-pages.
+
+# This hool will look at all sections in all pages, and if a sections
+# contains entry 'menu_title', it will create a new menu item, pointing
+# to that sections and put it into the 'headers' menu.
+require 'logger'
+require 'yaml'
+
+log = Logger.new(STDOUT)
+
 Jekyll::Hooks.register :site, :post_read do |site, payload|
+  menus = site.data["menus"]["header"]
   site.pages.each do |page|
     unless page.data["sections"] == nil
       page.data["sections"].each do |section|
         if section["menu_title"] != nil and section["section_id"] != nil
-          site.data["menus"]["header"].push({
-            "url" => page.url + "#" + section["section_id"],
-            "title" => section["menu_title"]
-          })
+          title = section["menu_title"]
+          if ! _has_menu(menus, title)
+            log.debug("Adding section '#{title}' to the menu.")
+            menus.push({
+              "url" => page.url + "#" + section["section_id"],
+              "title" => title
+            })
+          end
         end
       end
     end
   end
+  log.debug("The final menu is:\n#{menus.to_yaml}")
+end
+
+def _has_menu(menus, title)
+  result = menus.select do |item|
+    item["title"] == title
+  end
+  return result.size > 0
 end
